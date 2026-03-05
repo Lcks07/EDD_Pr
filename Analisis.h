854371f299443a1a19356d5e32827984765691ff
Lucas Rincon Mican <114701177+Lcks07@users.noreply.github.com>
2026-03-05 09:49:42 -0500

    Create Analisis.h

---
+++ Analisis.h
@@ -0,0 +1,21 @@
+#ifndef ANALISIS_H
+#define ANALISIS_H
+#include <string>
+
+struct Analisis {
+    std::string tipo;
+    std::string objeto;
+    std::string comentario;
+
+    Analisis CrearAnalisis(std::string tipo, std::string objeto, std::string comentario);
+    std::string ObtenerTipoAnalisis();
+    std::string ObtenerObjeto();
+    std::string ObtenerComentario();
+    bool tieneComentario();
+
+
+};
+
+
+
+#endif //ANALISIS_H
