ef6b6878d7b8f0c9bd6c78b46b393a41fe0ac7c5
Lucas Rincon Mican <114701177+Lcks07@users.noreply.github.com>
2026-03-05 09:25:43 -0500

    Create Movimiento.h

---
+++ Movimiento.h
@@ -0,0 +1,22 @@
+#ifndef MOVIMIENTO_H
+#define MOVIMIENTO_H
+#include <string>
+
+struct Movimiento {
+    std::string tipo;
+    std::string unidad;
+    double magnitud;
+
+    Movimiento CrearMovimiento(std::string tipo, std::string uniddad, double magnitud);
+    std::string ObetenerTipo();
+    std::string ObetenerUnidad();
+    double ObetenerMagnitud();
+
+    double Magnitud_Metros(Movimiento m);
+    double Magnitud_randianes(Movimiento m);
+
+};
+
+
+
+#endif //MOVIMIENTO_H
