6bea42695247872cee3ec034d9ccbd8f526b99f3
Lucas Rincon Mican <114701177+Lcks07@users.noreply.github.com>
2026-03-05 09:26:22 -0500

    Create Movimiento.cpp

---
+++ Movimiento.cpp
@@ -0,0 +1,58 @@
+#include "Movimiento.h"
+#include <string>
+
+#include "Punto.h"
+
+Movimiento Movimiento::CrearMovimiento(std::string tipo, std::string unidad, double magnitud) {
+    Movimiento m;
+    m.unidad = unidad;
+    m.magnitud = magnitud;
+    m.tipo = tipo;
+    return m;
+}
+
+std::string Movimiento::ObetenerTipo() {
+    return tipo;
+}
+
+std::string Movimiento::ObetenerUnidad() {
+    return unidad;
+}
+
+double  Movimiento:: ObetenerMagnitud() {
+    return magnitud;
+}
+
+double Movimiento::Magnitud_Metros(Movimiento m) {
+    if (m.tipo=="avanzar") {
+            if (m.unidad=="cm") {
+                return m.magnitud*0.01;
+            }else if (m.unidad=="dm") {
+                return m.magnitud*0.1;
+            }else if (m.unidad=="m") {
+                return m.magnitud*1;
+            }else if (m.unidad=="km") {
+                return m.magnitud*1000;
+            }
+            else {
+                return -1;
+    }
+    }
+}
+
+double Movimiento::Magnitud_randianes(Movimiento m) {
+    constexpr double PI = 3.14159265358979323846;
+    if (m.tipo=="girar") {
+        if (m.unidad=="grd") {
+            return m.magnitud*PI/180;
+        }
+    else if (m.unidad=="rad") {
+        return m.magnitud;
+    }
+    else {
+        return -1;
+    }
+        }else {
+            return -1;
+        }
+}
