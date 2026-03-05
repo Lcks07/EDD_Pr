8efbadd51bee31a09020363e16dec870b733aa89
Lucas Rincon Mican <114701177+Lcks07@users.noreply.github.com>
2026-03-05 09:50:11 -0500

    Create Analisis.cpp

---
+++ Analisis.cpp
@@ -0,0 +1,28 @@
+
+#include "Analisis.h"
+#include <string>
+Analisis Analisis::CrearAnalisis(std::string tipo, std::string objeto, std::string comentario) {
+    Analisis a;
+    a.tipo=tipo;
+    a.objeto=objeto;
+    a.comentario=comentario;
+    return a;
+}
+
+std::string Analisis:: ObtenerTipoAnalisis() {
+    return tipo;
+}
+
+std::string Analisis:: ObtenerObjeto() {
+    return objeto;
+}
+std::string Analisis:: ObtenerComentario() {
+    return comentario;
+}
+bool Analisis:: tieneComentario() {
+    if (comentario.empty()) {
+        return false;
+    }else{
+        return true;
+    }
+}
