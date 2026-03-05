2ecd4da7a8c0912f18dad411a26f64930b0d7a65
Lucas Rincon Mican <114701177+Lcks07@users.noreply.github.com>
2026-03-05 10:24:17 -0500

    Create Comando.cpp

---
+++ Comando.cpp
@@ -0,0 +1,39 @@
+#include "Comando.h"
+#include<string>
+
+Comando Comando:: CrearComandoMovimiento( Movimiento m) {
+    Comando c;
+    c.discriminador="Movimiento";
+    c.m=m;
+    return c;
+}
+
+Comando Comando:: CrearComandoAnalisis(Analisis a) {
+    Comando c;
+    c.discriminador="Analisis";
+    c.a=a;
+    return c;
+}
+
+bool Comando:: esMovimiento() {
+    if (discriminador=="Movimiento") {
+        return true;
+    }else {
+        return false;
+    }
+}
+bool Comando:: esAnalisis() {
+    if (discriminador=="Analisis") {
+        return true;
+    }else {
+        return false;
+    }
+}
+
+Movimiento Comando:: ObtenerMovimiento() {
+    return m;
+}
+
+Analisis Comando:: ObtenerAnalisis() {
+    return a;
+}
