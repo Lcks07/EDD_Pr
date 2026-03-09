08fbd3b1bbbe172495dcff03533af6ec65275646
iTorres30i <alejobarragan64@gmail.com>
2026-03-08 19:44:26 -0500

    Add ListaElementos class for managing elements

---
+++ lista_elementos.h
@@ -0,0 +1,29 @@
+#include <list>
+#include "elemento_interes.h"
+
+using namespace std;
+
+class ListaElementos {
+
+private:
+
+    list<ElementoInteres> elementos;
+
+public:
+
+    // Constructor
+    ListaElementos();
+
+    // Operaciones del TAD
+    void AgregarElemento(ElementoInteres elemento);
+
+    ElementoInteres ObtenerElemento(int posicion);
+
+    int TamanioElementos();
+
+    bool EstaVaciaElementos();
+
+    void LimpiarElementos();
+};
+
+#endif
